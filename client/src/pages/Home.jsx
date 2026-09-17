b57ce3d5a6b1d5aead301a8d7f0408be2287fcc6
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-09-17 16:52:28 +0200

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,8 +13,56 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8000/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter((item) =>
+          item.name
+            .toLowerCase()
+            .includes(
+              searchText.toLowerCase() ||
+                item.prompt.toLowerCase().includes(searchText.toLowerCase()),
+            ),
+        );
+
+        setSearchedResults(searchResults);
+      }, 500),
+    );
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -27,7 +75,14 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -45,12 +100,9 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards
-                  data={[]}
-                  title="No search results found"
-                />
+                <RenderCards data={searchedResults} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -17,4 +17,37 @@ router.route("/").get((req, res) => {
     res.send("Hello from Post Routes")
 })
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+//get route for all posts
+router.route("/").get(async(req, res)=>{
+    try{
+        const posts = await Post.find({})
+        res.status(200).json({success: true, data: posts})
+    } catch(error){
+        res.status(500).json({success: false, message: error.message})
+    }
+})
+
+//create post route
+router.route("/").post(async(req, res)=>{
+    try{
+        const {name, prompt, photo} = req.body
+        const photoUrl = await cloudinary.uploader.upload(photo)
+
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+        res.status(201).json({success: true, data: newPost})
+    } catch(error){
+        res.status(500).json({success: false, message: error.message})
+    }
+})
+
 export default router
